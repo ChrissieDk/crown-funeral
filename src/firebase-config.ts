df342b49a858bc21deee1a29d76e7729f8635e13
Christiaan de Kock <christiaan@getsavvi.co.za>
2024-10-14 11:33:29 +0200

    fixes

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, logEvent } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,3 +18,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+logEvent(analytics, "app_initialized");
