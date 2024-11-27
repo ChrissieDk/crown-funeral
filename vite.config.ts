e3a6115aab0bb6c7befd5cf03b08afeb96b47a8f
Christiaan de Kock <christiaan@getsavvi.co.za>
2024-11-27 09:42:37 +0200

    remove proxy

--- vite.config.ts
+++ vite.config.ts
@@ -11,23 +11,4 @@ export default defineConfig({
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  server: {
-    proxy: {
-      "/pol360": {
-        target: "https://web09.pol360.co.za",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/pol360/, ""),
-        secure: false,
-        configure: (proxy, _options) => {
-          proxy.on("proxyReq", (proxyReq, req, _res) => {
-            // Ensure headers are properly set
-            const authHeader = req.headers["authorization"];
-            if (authHeader) {
-              proxyReq.setHeader("Authorization", authHeader);
-            }
-          });
-        },
-      },
-    },
-  },
 });
